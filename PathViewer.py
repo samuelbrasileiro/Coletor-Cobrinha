cd4e509819c22884b45aa5936b76d7e528a31bf1
tta13 <tta@cin.ufpe.br>
2022-02-27 12:34:27 -0300

    Resetando tiles pintados corretamente em PathViewer.py

--- PathViewer.py
+++ PathViewer.py
@@ -9,37 +9,33 @@ class PathViewer:
     def __init__(self, map):
         self.map = map
         self.newColors = {}
-        self.oldColors = {}
         
     def paint(self, x, y, newColor):
         """
         Fills rectangle centered at (x, y) with specified color
-        """
-        self.oldColors[(x, y)] = self.map.getColor(x, y)        
+        """   
         self.newColors[(x, y)] = newColor
         
     def paintExploredNode(self, x, y):
         """
         Paints a node with default color for explored nodes
-        """
-        self.oldColors[(x, y)] = self.map.getColor(x, y)        
+        """     
         self.newColors[(x, y)] = EXPLORED_COLOR
         
     def paintPathNode(self, x, y):
         """
         Paints a node with default color for path nodes
         """
-        self.oldColors[(x, y)] = self.map.getColor(x, y)        
         self.newColors[(x, y)] = PATH_COLOR
         
     def restoreColor(self, x, y):
-        if (x, y) in self.oldColors:
-            self.newColors[(x, y)] = self.oldColors[(x,y)]
+        if (x, y) in self.newColors:
+            self.newColors.pop((x,y))
             
             
     def resetColors(self):
-        for k in self.oldColors.keys():
-            self.newColors[k] = self.oldColors[k]
+        for k in self.newColors.keys():
+            self.newColors.pop(k)
             
     def display(self):
         for (i, j) in self.newColors.keys():
