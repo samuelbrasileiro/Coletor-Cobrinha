e3e3c17aa728c50874972b8619ec67a90029a411
Amanda Moraes <amandascm23@gmail.com>
2022-03-03 10:09:31 -0300

    Fix classe XFS incluindo arquivo Container com classes Queue e Stack

---
+++ Container.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.elements = deque()
+    
+    def empty(self):
+        return not self.elements
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.pop()
+
+class Queue:
+    def __init__(self):
+        self.elements = deque()
+    
+    def empty(self):
+        return not self.elements
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.popleft()
